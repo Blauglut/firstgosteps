3da8ca513db771a5893d47ad77157d869cafd947
Thorfix <63151824+Blauglut@users.noreply.github.com>
2023-04-14 19:26:47 +0200

    Add files via upload

---
+++ package.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//fmt.Println("Lieblingsnummer ist: ", rand.Intn(13))
+	//}
+
+	fmt.Println(add(4, 5))
+}
+
+func add(x, y int) int {
+	return x + y
+}
